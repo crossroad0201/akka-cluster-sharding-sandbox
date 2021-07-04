52ddec81e2d9c63a32c6a7a5f71e88b7e21249ba
Yohei TSUJI <crossroad0201@gmail.com>
2021-07-04 20:01:19 +0900

    TODOコメント追記

--- src/main/scala/crossroad0201/sandbox/akkaclustersharding/package.scala
+++ src/main/scala/crossroad0201/sandbox/akkaclustersharding/package.scala
@@ -1,5 +1,9 @@
 package crossroad0201.sandbox
 
+// TODO Kamonを導入してメトリクスを取得できるようにする
+// TODO メトリクスをダッシュボードで可視化する
+// TODO 負荷試験できるようにする
+
 package object akkaclustersharding {
 
   type EntityId = String
